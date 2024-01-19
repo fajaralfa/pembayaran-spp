232088d393b26245411f5f68ed5a15f03907f88d
fajaralfa <fajaralfa@gmail.com>
2024-01-19 13:15:38 +0700

    membuat hyperlink ke home di navbar

--- layout/header_root.php
+++ layout/header_root.php
@@ -12,19 +12,20 @@
 <body>
     <nav class="navbar navbar-expand-lg" style="background-color: #e3f2fd;">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Pembayaran SPP</a>
+            <a class="navbar-brand" href="./">Pembayaran SPP</a>
             <div class="d-flex align-items-center lead">
                 <span><?= $_SESSION['spp_nama_petugas'] . ' | ' . $_SESSION['spp_level_petugas'] ?></span>
-                <a onclick="return confirm('Anda ingin keluar?')" href="../logout.php" class="btn">Logout</a>
+                <a onclick="return confirm('Anda ingin keluar?')" href="logout.php" class="btn">Logout</a>
             </div>
         </div>
     </nav>
     <div class="sidenav position-fixed z-1 overflow-x-hidden my-3 p-3" style="width: 180px; left: 10px; background: #eef;">
         <div class="navbar-nav">
+            <a class="nav-link" href="pembayaran/pilih_siswa.php">Pembayaran</a>
             <a class="nav-link" href="siswa/">Data Siswa</a>
+            <a class="nav-link" href="spp/">Data SPP</a>
             <a class="nav-link" href="kelas/">Data Kelas</a>
             <a class="nav-link" href="petugas/">Data Petugas</a>
-            <a class="nav-link" href="spp/">Data SPP</a>
         </div>
     </div>
 
